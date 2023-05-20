44ef37a99cbce85f437126c24b4a8a8d81b5aef0
AlejoGreco <uki.greco@gmail.com>
2023-05-20 17:00:19 -0300

    Testing digital outputs

--- tp1/tp1.ino
+++ tp1/tp1.ino
@@ -1,9 +1,16 @@
+
+#define ON_BOARD_LED 2
+
 void setup() {
   Serial.begin(115200);
+
+  pinMode(ON_BOARD_LED, OUTPUT);
 }
 
 void loop() {
   Serial.println("TEST");
-  Serial.println("");
+  digitalWrite(ON_BOARD_LED, HIGH);
+  delay(1000);
+  digitalWrite(ON_BOARD_LED, LOW);
   delay(1000);
 }
